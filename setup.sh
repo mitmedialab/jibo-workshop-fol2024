7f9e5317238cb31f3988cac888b80e1c0d23be83
Jon Ferguson <github@irontech.org>
2024-01-23 18:07:46 +0000

    fixed a few problems

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e -x
+
 apt update
 apt install -y software-properties-common
-add-apt-repository universe
+add-apt-repository -y universe
 apt update
 apt install -y curl
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-releaseecho $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release; echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update
 apt install -y ros-dev-tools
 apt upgrade -y
