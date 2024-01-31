08f5f2f6d2a3b2f3876e9728c65f70004ffe2e9d
Jon Ferguson <jon@jef.us>
2024-01-31 18:36:43 -0500

    removed the no longer needed activate script (yay!), cleaned up some of the container build stuff

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source /opt/ros/iron/setup.bash
 source /home/vscode/ros2_workspace/install/setup.bash 
 
--- container/launch.sh
+++ container/launch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#cd /workspaces/jibo-workshop-fol2024
 cd /home/vscode
-#source ./activate
 source /opt/ros/iron/setup.bash 
 source /home/vscode/ros2_workspace/install/setup.bash
 
